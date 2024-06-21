b6fe72b2cd52be0a268cdfd3988a74cefd200a85
JacksonSalyards <95992718+Thatthiccckid47@users.noreply.github.com>
2024-06-21 18:39:08 -0400

    finished basic database implementation

--- training.py
+++ training.py
@@ -3,7 +3,6 @@ import sqlite3
 import json
 from tensorflow.keras.models import Sequential
 
-
 timeframe = '2015-05'
 sql_transaction = []
 
@@ -14,10 +13,34 @@ def create_table():
     c.execute("""CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, 
                  comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)""")
 
-
 def formatData(data):
     data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
     return data
+
+def transactionBuilder (sql):
+    global sql_transaction
+    sql_transaction.append(sql)
+    if len(sql_transaction) > 1000:
+        c.execute('BEGIN TRANSACTION')
+        for s in sql_transaction:
+            try:
+                c.execute(s)
+            except:
+                pass
+        connection.commit()
+        sql_transaction = []
+    
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]':
+        return False
+    elif data == '[removed]':
+        return False
+    else:
+        return True
 
 def findParent(parentId):
     try:
@@ -32,11 +55,31 @@ def findParent(parentId):
         return False
 
 def sqlReplaceComment(comment_id, parent_id, comment, parent, subreddit, time, score):
-    try:
-        sql = """UPDATE parent_reply SET """
-
+   try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ? WHERE parent_id =?;""".format(parentid, commentid, parent, comment, subreddit, int(time), score, parentid)
+        transactionBuilder(sql)
     except Exception as e:
-        print("Replacemnt failure: ", str(e))
+        print('s0 insertion',str(e))
+
+def sqlInsertParent(comment_id, parent_id, comment, parent, subreddit, time, score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}","{}",{},{});""".format(parentid, commentid, parent, comment, subreddit, int(time), score)
+        transactionBuilder(sql)
+    except Exception as e:
+        print('s0 insertion',str(e))
+
+def sqlInsertNoParent(comment_id, parent_id, comment, parent, subreddit, time, score):
+   try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}",{},{});""".format(parentid, commentid, comment, subreddit, int(time), score)
+        transactionBuilder(sql)
+    except Exception as e:
+        print('s0 insertion',str(e))
+
+def filterData():
+    #TODO
+
+    #Will filter all inapropiate language 
+
 
 if __name__ == "__main__":`
     create_table()
@@ -47,6 +90,7 @@ if __name__ == "__main__":`
             row_counter += 1
             row - json.loads(row)
             parent_id = row['parent_id']
+            comment_id = row['name']
             body = formatData(row['body'])
             created_utc = row['created_utc']
             score = row['score']
@@ -58,11 +102,20 @@ if __name__ == "__main__":`
                     existingCommentScore = findExistingScore(parent_id)
                     if existingCommentScore:
                         if score > existingCommentScore:
+                                sqlReplaceComment(comment_id, parent_id, body, parent_data, subreddit, created_utc, score)
                             #Replace Comment In database with commentID, parentID, parentData, body, subreddit, score, created UTC
+                            
                     else:
                         if parent_data
                             #Insert that has parent with parentID, commentID, parentData, body, subreddit, score, created UTC
+                            sqlInsertParent(comment_id, parent_id, body, parent_data, subreddit, created_utc, score)
+                            paired_rows += 1
                         else
                             #Insert no parent with commetID, parentID, body, subreddiy, created UTC, score <-- might be someone else parent, so its needed.
+                            sqlInsertNoParent(comment_id, parent_id, body, subreddit, created_utc, score)
+           
+            if row_counter % 100000 == 0:
+                print('Total Rows Read: {}, Paired Rows: {}, Time: {}'.format(row_counter, paired_rows, str(datetime.now())))
+                            
 
 
