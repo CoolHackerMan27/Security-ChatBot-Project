c5b35aaf1298219990db2cfbdef2b6157ec98723
JacksonSalyards <95992718+Thatthiccckid47@users.noreply.github.com>
2024-06-27 10:44:07 -0400

    Added try catch to trainingDatabase

--- trainingDataSet.py
+++ trainingDataSet.py
@@ -19,40 +19,43 @@ timeframes = ['2007-07', '2007-08', '2007-09', '2007-10', '2007-11', '2007-12',
     '2015-01', '2015-02', '2015-03', '2015-04', '2015-05']
 
 for timeframe in timeframes:
-    connection = sqlite3.connect('{}.db'.format(timeframe))
-    c = connection.cursor()
-    limit = 5000
-    last_unix = 0
-    cur_length = limit
-    counter = 0
-    test_done = False
+    try:
+            connection = sqlite3.connect('{}.db'.format(timeframe))
+            c = connection.cursor()
+            limit = 5000
+            last_unix = 0
+            cur_length = limit
+            counter = 0
+            test_done = False
 
-    while cur_length == limit:
+            while cur_length == limit:
 
-        df = pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} and parent NOT NULL and score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix,limit),connection)
-        last_unix = df.tail(1)['unix'].values[0]
-        cur_length = len(df)
+                df = pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} and parent NOT NULL and score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix,limit),connection)
+                last_unix = df.tail(1)['unix'].values[0]
+                cur_length = len(df)
 
-        if not test_done:
-            with open('test.from','a', encoding='utf8') as f:
-                for content in df['parent'].values:
-                    f.write(content+'\n')
+                if not test_done:
+                    with open('test.from','a', encoding='utf8') as f:
+                        for content in df['parent'].values:
+                            f.write(content+'\n')
 
-            with open('test.to','a', encoding='utf8') as f:
-                for content in df['comment'].values:
-                    f.write(str(content)+'\n')
+                    with open('test.to','a', encoding='utf8') as f:
+                        for content in df['comment'].values:
+                            f.write(str(content)+'\n')
 
-            test_done = True
+                    test_done = True
 
-        else:
-            with open('train.from','a', encoding='utf8') as f:
-                for content in df['parent'].values:
-                    f.write(content+'\n')
+                else:
+                    with open('train.from','a', encoding='utf8') as f:
+                        for content in df['parent'].values:
+                            f.write(content+'\n')
 
-            with open('train.to','a', encoding='utf8') as f:
-                for content in df['comment'].values:
-                    f.write(str(content)+'\n')
+                    with open('train.to','a', encoding='utf8') as f:
+                        for content in df['comment'].values:
+                            f.write(str(content)+'\n')
 
-        counter += 1
-        if counter % 20 == 0:
-            print(counter*limit,'rows completed so far')
+                counter += 1
+                if counter % 20 == 0:
+                    print(counter*limit,'rows completed so far')
+    except Exception as e:
+        print(str(e))
