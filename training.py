783116b836552264f926757aeb7ee208fe24cdcc
JacksonSalyards <95992718+CoolHackerMan27@users.noreply.github.com>
2024-06-28 12:49:18 -0400

    added a Encoder and Decoder class that was AI generated lol, im feeling extra lazy rn!

--- training.py
+++ training.py
@@ -27,24 +27,144 @@ PE_TARGET = 1000
 # Transformer model class
 
 
+class EncoderLayer(Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = tf.keras.Sequential([
+            Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+            Dense(d_model)  # (batch_size, seq_len, d_model)
+        ])
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.dropout1 = Dropout(rate)
+        self.dropout2 = Dropout(rate)
+
+    def call(self, x, training, mask):
+        attn_output = self.mha(x, x, x, mask)  # (batch_size, seq_len, d_model)
+        attn_output = self.dropout1(attn_output, training=training)
+        # (batch_size, seq_len, d_model)
+        out1 = self.layernorm1(x + attn_output)
+
+        ffn_output = self.ffn(out1)  # (batch_size, seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        # (batch_size, seq_len, d_model)
+        out2 = self.layernorm2(out1 + ffn_output)
+
+        return out2
+
+
+class Encoder(Layer):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, maximum_position_encoding, rate=0.1):
+        super(Encoder, self).__init__()
+        self.d_model = d_model
+        self.num_layers = num_layers
+        self.embedding = Embedding(input_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(
+            maximum_position_encoding, d_model)
+        self.enc_layers = [EncoderLayer(
+            d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.dropout = Dropout(rate)
+
+    def call(self, x, training, mask):
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x)  # (batch_size, seq_len, d_model)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x, training=training)
+
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, training, mask)
+
+        return x  # (batch_size, seq_len, d_model)
+
+
+class DecoderLayer(Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(DecoderLayer, self).__init__()
+        self.mha1 = MultiHeadAttention(d_model, num_heads)
+        self.mha2 = MultiHeadAttention(d_model, num_heads)
+        self.ffn = tf.keras.Sequential([
+            Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+            Dense(d_model)  # (batch_size, seq_len, d_model)
+        ])
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = LayerNormalization(epsilon=1e-6)
+        self.dropout1 = Dropout(rate)
+        self.dropout2 = Dropout(rate)
+        self.dropout3 = Dropout(rate)
+
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        # (batch_size, target_seq_len, d_model)
+        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(attn1 + x)
+
+        attn2, attn_weights_block2 = self.mha2(
+            # (batch_size, target_seq_len, d_model)
+            enc_output, enc_output, out1, padding_mask)
+        attn2 = self.dropout2(attn2, training=training)
+        # (batch_size, target_seq_len, d_model)
+        out2 = self.layernorm2(attn2 + out1)
+
+        ffn_output = self.ffn(out2)  # (batch_size, target_seq_len, d_model)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        # (batch_size, target_seq_len, d_model)
+        out3 = self.layernorm3(ffn_output + out2)
+
+        return out3, attn_weights_block1, attn_weights_block2
+
+
+class Decoder(Layer):
+    def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size, maximum_position_encoding, rate=0.1):
+        super(Decoder, self).__init__()
+        self.d_model = d_model
+        self.num_layers = num_layers
+        self.embedding = Embedding(target_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(
+            maximum_position_encoding, d_model)
+        self.dec_layers = [DecoderLayer(
+            d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.dropout = Dropout(rate)
+
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        seq_len = tf.shape(x)[1]
+        attention_weights = {}
+        x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x, training=training)
+
+        for i in range(self.num_layers):
+            x, block1, block2 = self.dec_layers[i](
+                x, enc_output, training, look_ahead_mask, padding_mask)
+            attention_weights[f'decoder_layer{i+1}_block1'] = block1
+            attention_weights[f'decoder_layer{i+1}_block2'] = block2
+
+        return x, attention_weights
+
+
+def positional_encoding(position, d_model):
+    # Positional encoding implementation
+    pass
+
+
 class TransformerModel(tf.keras.Model):
-    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
-                 target_vocab_size, pe_input, pe_target, rate=0.1):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, target_vocab_size, pe_input, pe_target, rate=0.1):
         super(TransformerModel, self).__init__()
-        self.encoder = Encoder(num_layers, d_model, num_heads, dff,
-                               input_vocab_size, pe_input, rate)
-        self.decoder = Decoder(num_layers, d_model, num_heads, dff,
-                               target_vocab_size, pe_target, rate)
+        self.encoder = Encoder(
+            num_layers, d_model, num_heads, dff, input_vocab_size, pe_input, rate)
+        self.decoder = Decoder(
+            num_layers, d_model, num_heads, dff, target_vocab_size, pe_target, rate)
         self.final_layer = tf.keras.layers.Dense(target_vocab_size)
 
-    def call(self, inputs, targets, training, enc_padding_mask,
-             look_ahead_mask, dec_padding_mask):
+    def call(self, inputs, targets, training, enc_padding_mask, look_ahead_mask, dec_padding_mask):
         enc_output = self.encoder(inputs, training, enc_padding_mask)
         dec_output, attention_weights = self.decoder(
             targets, enc_output, training, look_ahead_mask, dec_padding_mask)
         final_output = self.final_layer(dec_output)
         return final_output, attention_weights
-
 # Tokenize and encode the data
 
 
