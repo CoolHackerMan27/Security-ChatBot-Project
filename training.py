f650fe66b982b9525c2425ea98114520f567bc7b
JacksonSalyards <95992718+Thatthiccckid47@users.noreply.github.com>
2024-07-01 09:15:22 -0400

    Updated encodelayer, decoderlayer, and multiheadattention class

--- training.py
+++ training.py
@@ -49,43 +49,28 @@ class MultiHeadAttention(Layer):
         x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
         return tf.transpose(x, perm=[0, 2, 1, 3])
 
-def call(self, inputs, targets, training=False, enc_padding_mask=None, look_ahead_mask=None, dec_padding_mask=None):
-    enc_output = self.encoder(inputs, training=training, mask=enc_padding_mask)
-    dec_output, attention_weights = self.decoder(
-        targets, enc_output, training=training, look_ahead_mask=look_ahead_mask, padding_mask=dec_padding_mask)
-    final_output = self.final_layer(dec_output)
-    return final_output, attention_weights
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
 
-    def scaled_dot_product_attention(self, q, k, v, mask):
-        matmul_qk = tf.matmul(q, k, transpose_b=True)
-        dk = tf.cast(tf.shape(k)[-1], tf.float32)
-        scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)  # (batch_size, seq_len, d_model)
+        v = self.wv(v)  # (batch_size, seq_len, d_model)
 
-        if mask is not None:
-            scaled_attention_logits += (mask * -1e9)
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
 
-        attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
-        output = tf.matmul(attention_weights, v)
+        scaled_attention, attention_weights = self.scaled_dot_product_attention(
+            q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)
+
+        concat_attention = tf.reshape(scaled_attention, 
+                                      (batch_size, -1, self.d_model))  # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
 
         return output, attention_weights
-
-
-def positional_encoding(position, d_model):
-    angle_rads = get_angles(np.arange(position)[:, np.newaxis], np.arange(d_model)[
-                            np.newaxis, :], d_model)
-
-    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
-    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
-
-    pos_encoding = angle_rads[np.newaxis, ...]
-
-    return tf.cast(pos_encoding, dtype=tf.float32)
-
-
-def get_angles(pos, i, d_model):
-    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
-    return pos * angle_rates
-
 
 class EncoderLayer(Layer):
     def __init__(self, d_model, num_heads, dff, rate=0.1):
@@ -100,17 +85,16 @@ class EncoderLayer(Layer):
         self.dropout1 = Dropout(rate)
         self.dropout2 = Dropout(rate)
 
-    def call(self, x, training, mask):
-        attn_output, _ = self.mha(x, x, x, mask)
+    def call(self, x, training=False, mask=None):
+        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
         attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(x + attn_output)
+        out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
 
-        ffn_output = self.ffn(out1)
+        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
         ffn_output = self.dropout2(ffn_output, training=training)
-        out2 = self.layernorm2(out1 + ffn_output)
+        out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
 
         return out2
-
 
 class Encoder(Layer):
     def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, maximum_position_encoding, rate=0.1):
@@ -150,22 +134,21 @@ class DecoderLayer(Layer):
         self.dropout2 = Dropout(rate)
         self.dropout3 = Dropout(rate)
 
-    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
-        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
+    def call(self, x, enc_output, training=False, look_ahead_mask=None, padding_mask=None):
+        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)  # (batch_size, target_seq_len, d_model)
         attn1 = self.dropout1(attn1, training=training)
         out1 = self.layernorm1(x + attn1)
 
         attn2, attn_weights_block2 = self.mha2(
-            enc_output, enc_output, out1, padding_mask)
+            enc_output, enc_output, out1, padding_mask)  # (batch_size, target_seq_len, d_model)
         attn2 = self.dropout2(attn2, training=training)
         out2 = self.layernorm2(out1 + attn2)
 
-        ffn_output = self.ffn(out2)
+        ffn_output = self.ffn(out2)  # (batch_size, target_seq_len, d_model)
         ffn_output = self.dropout3(ffn_output, training=training)
-        out3 = self.layernorm3(out2 + ffn_output)
+        out3 = self.layernorm3(out2 + ffn_output)  # (batch_size, target_seq_len, d_model)
 
         return out3, attn_weights_block1, attn_weights_block2
-
 
 class Decoder(Layer):
     def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size, maximum_position_encoding, rate=0.1):
